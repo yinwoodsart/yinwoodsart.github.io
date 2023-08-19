8814d5fb98741d16b02bf0964594205348dcba95
Yin Woods <info@yinwoods.com>
2023-08-19 01:55:43 +0100

    Add home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,26 @@
+import './Home.css';
+
 export const Home = () => {
-    return (<main>Home page</main>)
+    return (
+        <main className="home">
+            <h3>
+                <a href="https://vimeo.com/yinwoods">Vimeo </a>
+                <a href="https://www.youtube.com/yinwoods">Youtube</a>
+                <a href="https://giphy.com/yinwoods">GIPHY</a>
+                <a href="https://www.twitch.tv/yinwoods">twitch</a>
+                <a href="https://yinwoods.bandcamp.com">Bandcamp</a>
+                <a href="https://soundcloud.com/yinwoods">Soundcloud</a>
+                <a href="https://www.behance.net/yinwoods">Behance</a>
+                <a href="https://open.spotify.com/artist/6hzV0C9obuB36iZ8gPmYwK?si=SZqV2cSvTceVV75OZ-VNMA">Spotify </a>
+                <br />
+                <a href="https://www.goodreads.com/yinwoods">Goodreads</a>
+                <a href="https://letterboxd.com/yinwoods/">Letterboxd</a>
+                <a href="https://yinwoods.getarchive.net">Archives</a>
+                <a href="https://www.pinterest.com/yinwoods_/">Pinterest</a>
+                <a href="https://twitter.com/yinwoods_/">Twitter</a>
+                <br />
+                <a href="https://venmo.com/yinwoods">Venmo</a>
+                <a href="https://cash.app/$yinwoods">CASHAPP</a>
+            </h3>
+        </main>)
 }
