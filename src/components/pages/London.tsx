db288e03ee75406a12d7f6e8b6ea9c7cc01951bc
Yin Woods <info@yinwoods.com>
2023-08-20 14:20:43 +0100

    Add London photo page

--- src/components/pages/London.tsx
+++ src/components/pages/London.tsx
@@ -1,8 +1,19 @@
 import { useEffect } from "react";
+import london_1 from '../assets/images/london_1.jpeg';
+import london_2 from '../assets/images/london_2.jpeg';
+import './London.css';
+
 
 export const London = () => {
-    useEffect(() => {
-        document.title = 'London - Yin Woods';
-      }, []);
-    return (<main>London page</main>)
+  useEffect(() => {
+    document.title = 'London - Yin Woods';
+  }, []);
+  return (
+    <main className="london">
+      <div>Daily London</div>
+      <div className="photo"><img src={london_1} alt=''></img></div>
+      <div className="photo"><img src={london_2} alt=''></img></div>
+
+    </main>
+  )
 }
